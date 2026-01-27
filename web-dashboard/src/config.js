1d3ce581026bf04edaad31e17bcf573a3db098dc
Mr Barron <abdllahmasaudu@gmail.com>
2026-01-27 21:38:15 +0000

    Fix: Revert API URL to Azure Public IP for Remote Access

--- web-dashboard/src/config.js
+++ web-dashboard/src/config.js
@@ -2,5 +2,6 @@
 // When valid, use the Public IP of your Azure VM.
 // Example: http://20.55.123.45:8000
 
-export const API_BASE_URL = "http://127.0.0.1:8000";
-// CHANGE 'localhost' to your Azure VM Public IP above when deploying!
+// If accessing remotely, this MUST be the Public IP of your VM.
+export const API_BASE_URL = "http://20.164.0.114:8000";
+// If running locally on the VM, you can use "http://127.0.0.1:8000"
